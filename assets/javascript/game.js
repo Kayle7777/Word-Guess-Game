d54841e35c93448b881608997573ab480ab03f2c
Kayle7777 <jessewebb2@gmail.com>
2018-06-16 16:17:47 -0500

    Logic is hard

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,15 @@
 
 
 GameObject = {
-  eventListener: document.body.addEventListener('keyup', function(event) {
-        console.log(event.key);
-      }),
   words : ["Word","TestWord","MoreWords"],
+  eventListener: document.body.addEventListener('keyup', () => $("#test").html(event.key), false)
 }
+
+function omg(divs) {
+  selector = Math.floor(Math.random()*GameObject.words.length);
+  for(i=0;i<GameObject.words[selector].length;i++) {
+    $(document.body).append("_ ");
+  }
+}
+
+omg(GameObject.words)
