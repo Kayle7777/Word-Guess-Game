a2a6ac178f980dd34a7b98ef3352793a44dca60e
Kayle7777 <jessewebb2@gmail.com>
2018-06-16 16:22:39 -0500

    Notes for later

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,4 +12,7 @@ function omg(divs) {
   }
 }
 
+//notes to remember: put above function in object, and save "selector" number as a property on the object. Similar to past things with Python
+//Use that selector # in the future to get the word from the wordlist, and tie it to those created divs.
+
 omg(GameObject.words)
