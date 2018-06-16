074bbd1ef8dd6aeee32fc782672d87627fc2f040
Kayle7777 <jessewebb2@gmail.com>
2018-06-15 21:43:55 -0500

    Redesign, figuring out eventlisteners a little more

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,12 @@
-let key;
-
-window.onload = function() {
-  if (document.body) {
-    document.body.addEventListener('keyup', function(event) {
-      key = event.key
-    });
-  }
-}
-
 Game = {
-  keywatch : Game.watch(this.key, function(id, oldval, newval) {
-    console.log('key.' + id + ' changed from' + oldval + " to " + newval);
-    return newval;
-  })
-  this.key = key
+  key : null,
+  eventListener: window.addEventListener('load', function() {
+    if (document.body) {
+      document.body.addEventListener('keyup', function(event) {
+        this.key = event.key;
+        console.log(this.key);
+      })
+    }
+  }),
+  words : ["Word","TestWord","MoreWords"]
 }
