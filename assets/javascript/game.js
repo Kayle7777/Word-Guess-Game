26e276d0b9f134727f980e9760d83a4b47d0bc01
Kayle7777 <jessewebb2@gmail.com>
2018-06-17 10:21:17 -0500

    Got it creating new spans with specific IDs, for later use manipulating them

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,22 +2,23 @@
 
 GameObject = {
   words : ["Word","TestWord","MoreWords"],
-  eventListener: document.body.addEventListener('keyup', () => $("#test").html(event.key), false)
-}
-
-function createUnderscores(divs) {
-  selector = Math.floor(Math.random()*GameObject.words.length);
-  var textNode= "";
-  for(i=0;i<GameObject.words[selector].length;i++) {
-    textNode += "_ ";
-  }
-  var underscoreHolder = document.getElementById("underscoreHolder");
+  eventListener: document.body.addEventListener('keyup', () => $("#test").html(event.key), false),
+  createUnderscores: function(divs) {
+    this.selector = Math.floor(Math.random()*GameObject.words.length);
+    var underscoreHolder = document.getElementById("underscoreHolder");
+    // Erase whatever is there already when this runs
     underscoreHolder.innerHTML = "";
-    underscoreHolder.appendChild(document.createTextNode(textNode));
+    for(i=0;i<this.words[this.selector].length;i++) {
+      var x = document.createElement('span');
+      var y = document.createTextNode("_ ");
+      x.setAttribute("id","Span"+i);
+      x.appendChild(y);
+      underscoreHolder.appendChild(x);
+    }
+      // underscoreHolder.appendChild(document.createTextNode(textNode));
+  }
 }
-
-//notes to remember: put above function in object, and save "selector" number as a property on the object. Similar to past things with Python
 //Use that selector # in the future to get the word from the wordlist, and tie it to those created divs.
 
-createUnderscores(GameObject.words)
+GameObject.createUnderscores(GameObject.words)
 // need to have this delete the old one too CHECK got it
