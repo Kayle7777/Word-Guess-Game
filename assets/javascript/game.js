add8b5d6368e10c3aebbb10da4afa3eb9a986536
Kayle7777 <jessewebb2@gmail.com>
2018-06-17 10:03:28 -0500

    More stuff, good function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,14 +5,19 @@ GameObject = {
   eventListener: document.body.addEventListener('keyup', () => $("#test").html(event.key), false)
 }
 
-function omg(divs) {
+function createUnderscores(divs) {
   selector = Math.floor(Math.random()*GameObject.words.length);
+  var textNode= "";
   for(i=0;i<GameObject.words[selector].length;i++) {
-    $(document.body).append("_ ");
+    textNode += "_ ";
   }
+  var underscoreHolder = document.getElementById("underscoreHolder");
+    underscoreHolder.innerHTML = "";
+    underscoreHolder.appendChild(document.createTextNode(textNode));
 }
 
 //notes to remember: put above function in object, and save "selector" number as a property on the object. Similar to past things with Python
 //Use that selector # in the future to get the word from the wordlist, and tie it to those created divs.
 
-omg(GameObject.words)
+createUnderscores(GameObject.words)
+// need to have this delete the old one too CHECK got it
